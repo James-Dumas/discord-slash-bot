64293ee2cf60d7c5099c62775693521396228054
James Dumas <jamesdumas418@gmail.com>
2022-03-24 12:42:04 -0400

    add database

--- discord_slash_bot.py
+++ discord_slash_bot.py
@@ -1,9 +1,11 @@
-import discord, asyncio, os, json, time, traceback, socket
+import discord, asyncio, os, json, time, traceback, socket, glob
 from discord_slash import SlashCommand
 from aiohttp import client_exceptions
 from datetime import datetime
 from threading import Thread, Event
 from queue import Queue
+
+# functions
 
 def has_connection(host="8.8.8.8", port=53, timeout=3):
     """Check if there is an internet connection"""
@@ -16,6 +18,8 @@ def has_connection(host="8.8.8.8", port=53, timeout=3):
         return True
     except socket.error as ex:
         return False
+
+# Bot class
 
 class SlashBot(discord.Client):
     options = {
@@ -191,3 +195,173 @@ class SlashBot(discord.Client):
         self.__on_ready_tasks.append(func)
         return func
 
+# Database stuff
+
+class DatabaseClosedException(Exception):
+    def __init__(self):
+        super.__init__(self, "Cannot access database after it has been closed")
+
+class BotDatabase:
+
+    def __init__(self, db_dir, default_data={}, cache_size=10, io_sleep_interval=0.01):
+        if not os.path.isabs(db_dir):
+            db_dir = os.path.abspath(db_dir)
+
+        os.makedirs(db_dir, exist_ok=True)
+
+        self.db_dir = db_dir
+        self.default_data = default_data
+        self.cache_size = cache_size
+        self.io_sleep_interval = io_sleep_interval
+
+        self.stop = Event()
+        self.io_queue = Queue()
+        self.data_out = {}
+        self.cached_guilds = []
+        self.cache = {}
+        self.gid_locks = {}
+        self.gid_events = {}
+        prev_dir = os.getcwd()
+        os.chdir(self.db_dir)
+        for fn in glob.glob("*"):
+            self.gid_locks[fn] = asyncio.Lock()
+            self.gid_events[fn] = Event()
+
+        os.chdir(prev_dir)
+        self.io_thread = Thread(target=self.__data_io)
+        self.io_thread.daemon = True
+        self.io_thread.start()
+    
+    def reinit(self):
+        """Re-initialize the database if it was closed"""
+        if self.stop.is_set():
+            self.stop.clear()
+            self.io_thread.start()
+
+    def close(self):
+        """Close the database"""
+        self.stop.set()
+        self.io_thread.join()
+        self.data_out = {}
+        self.cache = {}
+        self.cached_guilds = []
+
+    def __data_io(self):
+        """Threaded function to write data on disk"""
+        while not self.stop.is_set() or not self.io_queue.empty():
+            sleep = True
+            if not self.io_queue.empty():
+                sleep = False
+                ts, guild_id, data = self.io_queue.get()
+                if data != None:
+                    # write data
+                    with open(os.path.join(self.db_dir, str(guild_id)), "w") as f:
+                        json.dump(data, f)
+
+                    self.gid_events[guild_id].set()
+                else:
+                    # read data
+                    with open(os.path.join(self.db_dir, str(guild_id)), "r") as f:
+                        data = json.load(f)
+                    
+                    self.data_out[ts] = data
+                    self.gid_events[guild_id].set()
+            
+            if sleep:
+                time.sleep(self.io_sleep_interval)
+        
+    async def __write(self, guild_id: str, data: dict):
+        if guild_id in self.cache:
+            self.cache[guild_id] = data
+
+        self.io_queue.put((time.time(), guild_id, data))
+        while not self.gid_events[guild_id].is_set():
+            await asyncio.sleep(self.io_sleep_interval)
+
+        self.gid_events[guild_id].clear()
+
+    async def __read(self, guild_id: str) -> dict:
+        if guild_id in self.cache:
+            self.cached_guilds.remove(guild_id)
+            self.cached_guilds.insert(0, guild_id)
+            return self.cache[guild_id]
+        else:
+            ts = time.time()
+            self.io_queue.put((ts, guild_id, None))
+            while not self.gid_events[guild_id].is_set():
+                await asyncio.sleep(self.io_sleep_interval)
+            
+            self.gid_events[guild_id].clear()
+            data = self.data_out.pop(ts)
+
+            if self.cache_size > 0:
+                self.cached_guilds.insert(0, guild_id)
+                self.cache[guild_id] = data
+
+            if self.cache_size >= 0 and len(self.cached_guilds) > self.cache_size:
+                del self.cache[self.cached_guilds.pop()]
+            
+            return data
+
+    async def get(self, guild: discord.Guild) -> dict:
+        """Retrieve a guild's database"""
+        if self.stop.is_set():
+            raise DatabaseClosedException()
+
+        gid = str(guild.id)
+        data = {}
+        if gid in self.gid_locks:
+            async with self.gid_locks[gid]:
+                data = await self.__read(gid)
+
+        data["id"] = gid
+        data["name"] = guild.name
+        for key in self.default_data:
+            if key not in data:
+                data[key] = self.default_data[key]
+
+        return data
+
+    async def put(self, guild: discord.Guild, data: dict):
+        """Put data into a guild's database"""
+        if self.stop.is_set():
+            raise DatabaseClosedException()
+
+        gid = str(guild.id)
+        file_not_exist = False
+        if gid not in self.gid_locks:
+            self.gid_locks[gid] = asyncio.Lock()
+            self.gid_events[gid] = Event()
+            file_not_exist = True
+
+        async with self.gid_locks[gid]:
+            if file_not_exist:
+                current_data = {}
+                current_data.update(self.default_data)
+                current_data["id"] = gid
+                current_data["name"] = guild.name
+            else:
+                current_data = await self.__read(gid)
+
+            current_data.update(data)
+            await self.__write(gid, current_data)
+    
+    async def delete(self, guild: discord.Guild, keys: tuple[str]) -> int:
+        """Delete items from a guild's database"""
+        if self.stop.is_set():
+            raise DatabaseClosedException()
+
+        gid = str(guild.id)
+        num_deleted = 0
+        if gid in self.gid_locks:
+            async with self.gid_locks[gid]:
+                current_data = await self.__read(gid)
+                for key in keys:
+                    if (key not in ("id", "name")) and (key not in self.default_data) and (key in current_data):
+                        del current_data[key]
+                        num_deleted += 1
+
+                if num_deleted > 0:
+                    await self.__write(gid, current_data)
+        
+        return num_deleted
--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 
 setup(
     name='discord_slash_bot',
-    version='1.2',
+    version='1.3',
     py_modules=['discord_slash_bot'],
 )
